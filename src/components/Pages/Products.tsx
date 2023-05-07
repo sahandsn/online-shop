bbf45d27d7162b6f4843e2dd4407905799133221
Sahand <sanaeisahand@gmail.com>
2023-05-07 12:51:19 +0430

    products re-fetched when changing pages

--- src/components/Pages/Products.tsx
+++ src/components/Pages/Products.tsx
@@ -1,15 +1,26 @@
-import SearchAndSort from '../SearchAndSort/SearchAndSort'
-import CardList from '../CardList/CardList'
-import { useAppSelector } from '../../hooks/hooks'
+import SearchAndSort from '../SearchAndSort/SearchAndSort';
+import CardList from '../CardList/CardList';
+import { useAppSelector, useAppDispatch } from '../../hooks/hooks';
+import { getAll } from '../../services/products';
+import { useEffect } from 'react';
+import { setAllProducts } from '../../reducers/productsReducer';
 
 const Products: React.FC = () => {
-  const { products } = useAppSelector((state) => state)
+  const { products } = useAppSelector((state) => state);
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    (async () => {
+      const response = await getAll();
+      dispatch(setAllProducts({productApi: response}))
+    })();
+  }, []);
 
   return (
     <>
       <SearchAndSort />
       <CardList productList={products} />
     </>
-  )
-}
-export default Products
+  );
+};
+export default Products;
